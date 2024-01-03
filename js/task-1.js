6c7ae4135b42cc2bdb266855cc3db72e833e6f7d
Tymofii <tymofii.wga@gmail.com>
2024-01-03 12:25:27 +0200

    ended task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,29 @@
 'use strict';
 
-const slugify = (title) => title.toLowerCase().split(' ').join('-');
+const isEnoughCapacity = (products = {}, containerSize) => {
+  let totalCountProducts = 0;
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+  for (const key in products) {
+    if (Object.hasOwnProperty.call(products, key)) {
+      totalCountProducts += products[key];
+    }
+  }
+
+  return totalCountProducts <= containerSize ? true : false;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
